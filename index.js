bbe58978547effaf46c237012997baa40b979431
Šimon David <simdav01@pinf.cz>
2020-12-11 12:47:08 +0100

    pridani writeFile- oprava 2

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ http.createServer(function (req, res) {
             fs.writeFile(newpath, data, function (err) {
                 if (err) throw err;
                 res.write('File uploaded and moved!');
-                res.end();
+                //res.end();
                 console.log('File written!');
             });
 
